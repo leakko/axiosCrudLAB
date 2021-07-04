bdb9cab505e3755f19e065b52b5d6bd76ed6af21
Marcos Salvo <marcossalvo95@gmail.com>
2021-07-04 14:59:15 +0200

    Script.js fixed

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,20 @@
 window.addEventListener('load', ()=> {
+
+  // Delete joke
+
   const buttonsCollection = document.getElementsByTagName("button");
   const buttons = Array.from(buttonsCollection);
   buttons.forEach(button => {
       button.addEventListener("click", () => {
           const id = button.value
-          axios.delete(`http://localhost:8000/jokes/${id}`);
-          button.parentElement.parentElement.parentElement.remove()
+          axios.delete(`http://localhost:8000/jokes/${id}`)
+          .then (() => {
+            button.parentElement.parentElement.parentElement.remove()
+          })
       })
   });
+
+  
+
+
 });
